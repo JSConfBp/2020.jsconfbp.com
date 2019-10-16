8e26dfae660559f1ef928688aa8b319280b74b13
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-10-16 10:03:05 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,10 @@ import './index.scss'
 const IndexPage = props => (
   <div className="home">
     <div className="logo"></div>
+
     <h1>JSConf Budapest 2020</h1>
     <h2>September 24-25</h2>
+
     <Button
       href="https://ti.to/jsconf-bp/jsconf-budapest-2020"
       block
@@ -20,9 +22,18 @@ const IndexPage = props => (
     </Button>
 
     <p className="info">
+      <a href="http://2015.jsconfbp.com/">2015</a> ∙
+      <a href="http://2016.jsconfbp.com/">2016</a> ∙
+      <a href="http://2017.jsconfbp.com/">2017</a> ∙
+      <a href="https://2019.jsconfbp.com/">2019</a>
+    </p>
+
+    <p className="info">
       Have questions, wish to sponsor us? Let us know:
       <br /> <a href="mailto:team@jsconfbp.com">team@jsconfbp.com</a>
     </p>
+
+
   </div>
 )
 
