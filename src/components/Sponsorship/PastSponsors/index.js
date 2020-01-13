3e2aac267df1c15aac07f7671b95b3a3cb79194e
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-01-13 13:31:09 +0100

    add sponsors to blogpost

--- src/components/Sponsorship/PastSponsors/index.js
+++ src/components/Sponsorship/PastSponsors/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import classnames from 'classnames'
 import SponsorImage from '../../SponsorImage'
+import SponsorList from './list'
 import data from './data'
 import styles from './pastsponsors.module.scss'
+
 
 const SponsorImageLink = ({ link, image, name }) => (
   <a href={link} title={name}>
@@ -16,51 +18,8 @@ const PastSponsors = () => (
       <div className={styles.block_inner}>
         <h2>Recent Sponsors </h2>
 
-        <ul
-          className={classnames(styles.sponsor_list, styles.sponsor_list_big)}
-        >
-          {data.big.map(sponsor => (
-            <li
-              className={styles[`sponsor_${sponsor.level}`]}
-              key={sponsor.name}
-            >
-              <SponsorImageLink {...sponsor} />
-            </li>
-          ))}
-        </ul>
-        <ul
-          className={classnames(
-            styles.sponsor_list,
-            styles.sponsor_list_medium
-          )}
-        >
-          {data.med.map(sponsor => (
-            <li
-              className={styles[`sponsor_${sponsor.level}`]}
-              key={sponsor.name}
-            >
-              <SponsorImageLink {...sponsor} />
-            </li>
-          ))}
-        </ul>
-        <ul className={styles.sponsor_list}>
-          {data.sm.map(sponsor => (
-            <li
-              className={styles[`sponsor_${sponsor.level}`]}
-              key={sponsor.name}
-            >
-              <SponsorImageLink {...sponsor} />
-            </li>
-          ))}
-        </ul>
+        <SponsorList />
 
-        <ul className={styles.sponsor_list}>
-          {data.scholarship.map(sponsor => (
-            <li className={styles.sponsor_scholarship} key={sponsor.name}>
-              <SponsorImageLink {...sponsor} />
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   </>
