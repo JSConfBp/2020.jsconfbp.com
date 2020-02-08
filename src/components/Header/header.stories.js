1d03b0c53b67866e66d8a7ce1ad0729aebce87ef
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-02-08 20:05:52 +0100

    header

--- src/components/Header/header.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Header from './index';
-
-export default { title: 'Header' };
-
-export const big = () => <Header />;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import css from './header.module.scss'
-import Triangles from './triangles'
-//import { Link } from 'gatsby'
-
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Img from "gatsby-image"
 import Logo from '../Logo'
 
-export default () => (<header className={ css.header }>
+export default () => {
+
+/*   const data = useStaticQuery(graphql`
+    query MyQuery {
+      file(relativePath: { eq: "images/hero.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data); */
+
+
+  return (<header className={ css.header }>
     <div className={ css.inner }>
       <div className={ css.title }>
         <Logo className={ css.logoSvg } polygonClassName={ css.polygons } />
 
-        <h1 className={ css.mainTitle }>
+        <h1 className={ css.jsconfBp }>
           <span className={ css.jsconf }>
             <span className={ css.logo }>JS</span>
             <span className={ css.conf }>Conf</span>
           </span>
-          <span className={ css.city }>Budapest</span>
+          <span className={ css.budapest }>Budapest</span>
         </h1>
 
         <div className={ css.date }>
@@ -24,9 +40,20 @@ export default () => (<header className={ css.header }>
             2020
           </span>
         </div>
-
       </div>
-      <nav className={ css.navigation }>
+
+      <div className={ css.hero }>
+        <div className={ css.mask }>
+          <div className={ css.content }>
+{/*             <Img
+              fluid={ data.file.childImageSharp.fluid }
+              alt=""
+            /> */}
+          </div>
+        </div>
+      </div>
+
+      {/* <nav className={ css.navigation }>
         <ul className={ css.menu }>
           <li><a href="">Tickets</a></li>
           <li><a href="">Speakers</a></li>
@@ -36,17 +63,20 @@ export default () => (<header className={ css.header }>
           <li><a href="">Workshops</a></li>
         </ul>
       </nav>
-
+ */}
     {/* <Link to="/">
       <div className={ css.logo }></div>
     </Link>
  */}
     </div>
 
-   {/*  <div className={ css.line1 }></div>
+
+{/*
+   <div className={ css.line1 }></div>
     <div className={ css.line2 }></div>
     <div className={ css.line3 }></div>
     <div className={ css.line4 }></div>
-    <div className={ css.line5 }></div> */}
+    <div className={ css.line5 }></div>
+     */}
   </header>)
-
+}
