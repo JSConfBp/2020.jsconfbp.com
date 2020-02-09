3eb0ed3b3cb029e555d78bd3d257b5cc83ef96cc
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-02-09 11:07:19 +0100

    footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,26 +7,50 @@ import css from './footer.module.scss'
 export default ({ className = '' }) => (
   <footer className={ classnames(className ,css.footer) }>
     <section>
-    <p>JSConf Budapest welcomes everybody, please be nice to each other.</p>
-    <nav>
-      <ul className="unstyled">
-        <li>
-          <Link to={'/code-of-conduct'}>Code of Conduct</Link>
-        </li>
-        <li>
-          <Link to={'/privacy-policy'}>Privacy Policy</Link>
-        </li>
-        <li>
-          <Link to={'/sponsorship'}>Sponsorship</Link>
-        </li>
-        <li>
-          <a href="https://twitter.com/jsconfbp">@jsconfbp</a>
-        </li>
-      </ul>
-    </nav>
+      <p>JSConf Budapest welcomes everybody, please be nice to each other.</p>
+      <nav>
+        <ul className="unstyled">
+          <li>
+            <Link to={'/code-of-conduct'}>About Us</Link>
+          </li>
+          <li>
+            <Link to={'/code-of-conduct'}>Code of Conduct</Link>
+          </li>
+          <li>
+            <Link to={'/privacy-policy'}>Privacy Policy</Link>
+          </li>
+          <li>
+            <Link to={'/sponsorship'}>Sponsorship</Link>
+          </li>
+          <li>
+            <Link to={'/code-of-conduct'}>Impressum</Link>
+          </li>
+          <li>
+            <a href="https://twitter.com/jsconfbp">@jsconfbp</a>
+          </li>
+        </ul>
+      </nav>
 
-    <Link to={'/'}><Logo className={ css.logo } polygonClassName={ css.logoFill } /></Link>
-    <small>JSConf Budapest 2020</small>
+      <Link to={'/'}>
+        <Logo className={ css.logo } polygonClassName={ css.logoFill } />
+      </Link>
+      <small className={ css.jsconfbp }>JSConf Budapest</small>
+      <nav>
+        <ul className={classnames("unstyled", css.years)}>
+        <li>
+            <a href="https://2015.jsconfbp.com">2015</a>
+          </li>
+          <li>
+            <a href="https://2016.jsconfbp.com">2016</a>
+          </li>
+          <li>
+           <a href="https://2017.jsconfbp.com">2017</a>
+          </li>
+          <li>
+            <a href="https://2019.jsconfbp.com">2019</a>
+          </li>
+        </ul>
+      </nav>
     </section>
   </footer>
 )
