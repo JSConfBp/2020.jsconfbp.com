1535671eaf22f3ed434772d50f153083c1a4cdb8
Zsolt Pentz <pentzzsolt@gmail.com>
2022-01-20 20:41:47 +0100

    Fix typo

--- src/pages/code-of-conduct.js
+++ src/pages/code-of-conduct.js
@@ -16,7 +16,7 @@ const CodeOfConduct = () => {
       <p>
         All attendees, speakers, sponsors and volunteers at JSConf Budapest are
         required to agree with the following code of conduct. Organizers will
-        enforce this code throughout the event. We are expecting coorporation
+        enforce this code throughout the event. We are expecting cooperation
         from all participants to help ensuring a safe environment for everybody.
       </p>
       <p>
